2bc728cea5c40d4f61080aac88e85babf1d8c7ff
darkknightraj <rajankumarm30@gmail.com>
2025-04-18 17:54:43 +0530

    why this

--- hi.py
+++ hi.py
@@ -2,3 +2,8 @@ print("with ssh ")
 print("Hi i am doing this again")
 print("today 18 april 2025")
 print("today i used the zsh:")
+def addition(a,b):
+    return a+b
+a=6
+b=9
+print(addition(a,b))
