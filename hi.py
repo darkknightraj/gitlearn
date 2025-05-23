14dd281af51f483c76b7d45566bf36b2855838c8
darkknightraj <rajankumarm30@gmail.com>
2025-05-23 22:34:28 +0530

    day may 23 2025

--- hi.py
+++ hi.py
@@ -2,6 +2,7 @@ print("with ssh ")
 print("Hi i am doing this again")
 print("today 18 april 2025")
 print("today i used the zsh:")
+pint("after long time again i used  the github")
 def addition(a,b):
     return a+b
 a=6
